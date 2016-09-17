4d30635445a826d2c79197cb8afa1d78fff5c33c
Jake Pusateri <jakepusateri@gmail.com>
2016-09-16 22:35:14 -0700

    Use built-in require cache instead of lazy-cache

--- index.js
+++ index.js
@@ -7,10 +7,8 @@
 
 'use strict';
 
-var utils = require('./utils');
-
 module.exports = function centerAlign(val) {
-  return utils.align(val, function (len, longest) {
+  return require('align-text')(val, function (len, longest) {
     return Math.floor((longest - len) / 2);
   });
 };
